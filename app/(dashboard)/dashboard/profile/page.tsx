ae51d50bb518e99d35a00cdde233b3bfd1642217
sanyogsr <sanyogsingrajput@gmail.com>
2024-11-16 17:56:18 +0530

    pages added

--- app/(dashboard)/dashboard/profile/page.tsx
+++ app/(dashboard)/dashboard/profile/page.tsx
@@ -116,7 +116,7 @@ const BillingHistory: React.FC<BillingHistoryProps> = ({
                           ? "bg-green-100 text-green-700"
                           : payment.status === "pending"
                           ? "bg-yellow-100 text-yellow-700"
-                          : "bg-red-100 text-red-700"
+                          : "bg-green-300 text-green-700"
                       }`}
                     >
                       {payment.status}
